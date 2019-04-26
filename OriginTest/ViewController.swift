51700fbcb93b47f13d49453b3b651f67b68e21ca
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2019-04-26 17:43:28 -0500

    shared group configured and url scheme

--- OriginTest/ViewController.swift
+++ OriginTest/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let service = "myService"
+    let account = "myAccount"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func launchMobileX(_ sender: Any) {
+        
+        //Meter la info a keychain y luego al grupo para compartirla
+        
+        
+        
+        KeychainService.savePassword(service: service, account: account, data: "12345")
+    
+        
+        
+        let userDefaults = UserDefaults(suiteName: "group.com.mobilex")!
+        userDefaults.set("12345", forKey: "DynamicVector")
+        userDefaults.synchronize()
+        
+        
+        
+        //openApp(appURL: "com.mobilex://")
+        openApp(appURL: "com.mobilexParameter://testColor?value1=red")
+        
+    }
+    
 
+    func openApp(appURL: String) {
+        let openApp = URL(string: appURL)!
+        
+        if UIApplication.shared.canOpenURL(openApp) {
+            UIApplication.shared.open(openApp, options: [:], completionHandler: { (success) in
+                print("Open App: \(appURL)")
+                print("Status: \(success)")
+            })
+        }
+    }
+    
 }
 
